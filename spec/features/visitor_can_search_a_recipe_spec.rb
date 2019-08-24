72d635220a2d20010463b4ecd5ae1446217eca09
Karina <kaformentini@gmail.com>
2019-08-24 18:12:38 -0300

    Busca sem resultados ok

--- spec/features/visitor_can_search_a_recipe_spec.rb
+++ spec/features/visitor_can_search_a_recipe_spec.rb
@@ -31,7 +31,7 @@ feature 'Visitor can search for a recipe' do
     
     end
 
-    scenario 'Successfully' do
+    scenario 'and can search for parts of recipe name' do
         recipe_type = RecipeType.create!(name: 'Sobremesa')
         cuisine = Cuisine.create!(cuisine_name: 'Brasileira')
         other_recipe_type = RecipeType.create!(name: 'Doces')
@@ -66,5 +66,42 @@ feature 'Visitor can search for a recipe' do
     
     end
 
+    scenario 'and can find any recipe and have a message' do
+        recipe_type = RecipeType.create!(name: 'Sobremesa')
+        cuisine = Cuisine.create!(cuisine_name: 'Brasileira')
+        other_recipe_type = RecipeType.create!(name: 'Doces')
+        other_cuisine = Cuisine.create!(cuisine_name: 'Caseira')
+        
+        recipe = Recipe.create!(title: 'Bolo de cenoura', difficulty: 'Médio',
+                                recipe_type: recipe_type, cuisine: cuisine,
+                                cook_time: 50,
+                                ingredients: 'Farinha, açucar, cenoura',
+                                cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+        other_recipe = Recipe.create!(title: 'Bolo de Banana', difficulty: 'Médio',
+                                recipe_type: other_recipe_type, cuisine: other_cuisine,
+                                cook_time: 50,
+                                ingredients: 'Farinha, açucar, cenoura',
+                                cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+    
+        cook_recipe = Recipe.create!(title: 'Cook de chocolate', difficulty: 'Médio',
+                                recipe_type: other_recipe_type, cuisine: other_cuisine,
+                                cook_time: 50,
+                                ingredients: 'Farinha, açucar, cenoura',
+                                cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+    
+        visit root_path
+
+        fill_in 'Buscar por', with: 'Biscoito'
+        click_on 'Buscar'
+
+        expect(page).not_to have_content('Bolo de cenoura')
+        expect(page).not_to have_content('Bolo de Banana')
+        expect(page).not_to have_content('Cook de chocolate')
+    
+    end
+
+
+    
 end
 
