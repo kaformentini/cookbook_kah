33939a5d52819d18ac772cbdfa14d0a5b76c6676
Karina <kaformentini@gmail.com>
2019-08-27 20:05:42 -0300

    Fotos podem ser adicionadas, e sáo visualizadas no iniciar e na receita

--- spec/features/user_can_upload_an_image_recipe_spec.rb
+++ spec/features/user_can_upload_an_image_recipe_spec.rb
@@ -2,21 +2,41 @@ require 'rails_helper'
 
 feature "User can upload an image to the recipe" do
     scenario "successfully" do
-    recipe_type = RecipeType.create!(name: 'Sobremesa')
-    cuisine = Cuisine.create!(cuisine_name: 'Brasileira')
-    
-    visite root_path
-    click_on 'Cadastrar Receita'
+        recipe_type = RecipeType.create!(name: 'Sobremesa')
+        cuisine = Cuisine.create!(cuisine_name: 'Brasileira')
+        
+        visit root_path
+        click_on 'Cadastrar Receita'
 
-    fill_in 'Título', with: 'Tabule'
-    select 'Entrada', from: 'Tipo da Receita'
-    select 'Arabe', from: 'Cozinha'
-    
-    fill_in 'Dificuldade', with: 'Fácil'
-    fill_in 'Tempo de Preparo', with: '45'
-    fill_in 'Ingredientes', with: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha'
-    fill_in 'Como Preparar', with: 'Misturar tudo e servir. Adicione limão a gosto.'
-    
-    click_on 'Enviar'
+        fill_in 'Título', with: 'Tabule'
+        select 'Sobremesa', from: 'Tipo da Receita'
+        select 'Brasileira', from: 'Cozinha'
+        attach_file("Imagem", Rails.root + '/home/kah/Área de Trabalho/índice.jpeg') 
+
+        fill_in 'Dificuldade', with: 'Fácil'
+        fill_in 'Tempo de Preparo', with: '45'
+        fill_in 'Ingredientes', with: 'Trigo para quibe, cebola, tomate picado, azeite, salsinha'
+        fill_in 'Como Preparar', with: 'Misturar tudo e servir. Adicione limão a gosto.'
+        
+        click_on 'Enviar'
+
+        ####expect(page).to have_content('índice.jpeg')
     end
+
+    scenario "and see the image in the root_path resume" do
+
+        recipe_type = RecipeType.create!(name: 'Sobremesa')
+        cuisine = Cuisine.create!(cuisine_name: 'Brasileira')
+        Recipe.create!(title: 'Bolodecenoura', difficulty: 'Médio',
+                        recipe_type: recipe_type, cuisine: cuisine,
+                        cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                        cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+    
+        
+        visit root_path
+        
+        ####expect(page).to have_content('índice.jpeg')
+
+    end
+    
 end
